c7063eea1f3ffc6c54ebf6d04b492f9830816b66
Mary Bowden <mary.bowden@defra.gsi.gov.uk>
2018-08-09 15:13:34 +0100

    Testing routes pages

--- app/routes.js
+++ app/routes.js
@@ -9,3 +9,17 @@ router.get('/', function (req, res) {
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.get('/tests/ac1', function (req, res) {
+    res.render('tests/routes-test',{
+      "routeType": "AC1",
+      "numAccounts":4
+    })
+})
+
+router.get('/tests/ac2', function (req, res) {
+    res.render('tests/routes-test',{
+      "routeType":"AC2",
+      "numAccounts":87
+    })
+})
