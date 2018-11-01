0cf9795e9edcbb60da830a1718a4bbaef3d3cd9c
Mary Bowden <mary.bowden@defra.gsi.gov.uk>
2018-11-01 10:45:45 +0000

    changes to variables for flows. Updating Service names

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service e.g. UK-REACH-IT',
+  serviceName: 'Use Defra online',
 
   // Default port that prototype runs on
   port: '3000',
