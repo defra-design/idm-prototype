01e0e081548f60fde1972c0161a870aa50602422
Mary Bowden <mary.bowden@defra.gsi.gov.uk>
2018-12-06 16:44:32 +0000

    Setting up testing scenarios

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ module.exports = {
   serviceName: 'Your Defra account',
 
   // Default port that prototype runs on
-  port: '3000',
+  port: '3001',
 
   // Enable or disable password protection on production
   useAuth: 'true',
