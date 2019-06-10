496595288bda700a6c96cfaba34db2ea826cac3a
Mary Bowden <mary.bowden@defra.gsi.gov.uk>
2019-06-10 13:02:21 +0100

    Change to default values and Update details

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -23,17 +23,24 @@ module.exports = {
 "orgName": "Acme Org Ltd",
 "numUsers": "5",
 "numChildUsers": "1",
-"numServices": "3",
+"numServices": "1",
 "numAgents": "0",
 "numLocations": "1",
-"service": "Chemicals",
+"service": "Imports",
 "serviceLine": "Comply with REACH IT",
 "serviceRole": "REACH Manager",
 "userType": "new",
 "subscribeService": "no",
 "accountAccess": "no",
 "verifiedUser": "none",
-"flowType": "task"
+"flowType": "task",
+"sub-building-number": "1b",
+"building-number": "12",
+"street-name": "The Grove",
+"address-town": "Mytown",
+"address-postcode": "MY3 4RF",
+"address-country": "UK"
+
 
 
   // Insert values here
