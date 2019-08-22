2f36809d46938eb04cf9551c602c5f9b222fb892
Mary Bowden <mary.bowden@defra.gsi.gov.uk>
2019-08-22 11:14:34 +0100

    updates

--- app/routes.js
+++ app/routes.js
@@ -130,6 +130,13 @@ router.get('/scp/login-complete', function (req, res) {
     })
 })
 
+// Existing admin, standard or agent - complete app
+router.get('/scp/login-account', function (req, res) {
+    res.render('scp/login',{
+      "signinButton": '/manage/team-members/services-on-team-member-admin-account_company'
+    })
+})
+
 // Existing user - pending app
 router.get('/scp/login-pending', function (req, res) {
     res.render('scp/login',{
